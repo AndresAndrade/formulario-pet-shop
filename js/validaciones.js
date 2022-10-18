50ba0a5597c7192ad259daec283a44a3e0335e55
Andres Andrade <afandradep@outlook.com>
2022-10-17 21:37:13 -0500

    Initial commit

---
+++ js/validaciones.js
@@ -0,0 +1,52 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+    }
+}
+
+const mensajesDeError = {
+    nombre: "Este campo no puede estar vacío",
+    email: {
+        valueMissing: "Este campo no puede estar vacío",
+        typeMismatch: "El correo no es valido"
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales"
+    },
+    nacimiento: {
+        valueMissing: "Este campo no puede estar vacío",
+        customError: "Debes tener al menos 18 años de edad"
+    }
+}
+
+const validadores = {
+    nacimiento: input => validarNacimiento(input)
+}
+
+function validarNacimiento(input) {
+    const fechaCliente = new Date(input.value);
+    let mensaje = "";
+
+    if(!mayorDeEdad(fechaCliente)) {
+        mensaje = "Debes tener al menos 18 años de edad";
+    }
+
+    input.setCustomValidity(mensaje);
+}
+
+function mayorDeEdad(fecha) {
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(
+        fecha.getUTCFullYear() + 18,
+        fecha.getUTCMonth(),
+        fecha.getUTCDate());
+    return diferenciaFechas <= fechaActual;
+}
